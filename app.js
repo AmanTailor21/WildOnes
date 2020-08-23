8cb3e3dfadd5ac1933f0c9d5d9df2ce52349a9f5
Aman Tailor <amantailor21@gmail.com>
2020-08-23 13:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', (req, res) => res.sendfile("index.html"))
+app.get('/', (req, res) => res.sendfile('index.html'))
 
 // Configure server 
 const port = process.env.PORT || 3000
